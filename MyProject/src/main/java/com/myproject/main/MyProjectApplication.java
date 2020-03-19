ac87ae2a20a67bbcac745ff08742d44be9c2f08e
rakhi <rakhi@Rakesh_Jaligama>
2020-03-18 19:37:03 -0700

    Renamed the package name

--- MyProject/src/main/java/com/myproject/main/MyProjectApplication.java
+++ MyProject/src/main/java/com/myproject/main/MyProjectApplication.java
@@ -1,4 +1,4 @@
-package com.myproject.problems;
+package com.myproject.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
